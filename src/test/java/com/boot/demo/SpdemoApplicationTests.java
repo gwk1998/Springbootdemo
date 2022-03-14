2452a3b794401b0e24db1b8b43c9ef51598ffd39
gewenkai <gewk@newland.com.cn>
2022-03-14 10:20:30 +0800

    Initial commit

---
+++ src/test/java/com/boot/demo/SpdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boot.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpdemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
